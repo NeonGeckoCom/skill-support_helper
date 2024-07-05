6659545b3f7f1fec3c0bd537d33f451e64c87ee9
Daniel McKnight <daniel@neon.ai>
2024-07-05 09:37:34 -0700

    Remove leftover debug logging

--- __init__.py
+++ __init__.py
@@ -25,7 +25,7 @@
 # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-import json
+
 import shutil
 import sys
 import yaml
@@ -141,7 +141,6 @@ class SupportSkill(NeonSkill):
                     encode_file_to_base64_string(file)
             except Exception as e:
                 LOG.exception(e)
-        LOG.debug(f"Attachments Size={len(json.dumps(attachments))}")
         return attachments
 
     def _format_email_body(self, diagnostics: dict) -> str:
